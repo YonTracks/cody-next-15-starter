f42cd7e5454562414bf810eb3c61d052c8a6c1f9
Clinton Renton <clinton.renton@yahoo.com>
2025-02-11 23:57:10 +1000

    init db

--- auth.ts
+++ auth.ts
@@ -1,21 +1,67 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import { z } from "zod";
+// import type { User } from "@/lib/definitions";
+// import { ensureUsersTableExists } from "./lib/actions";
+// import bcrypt from "bcrypt";
 
+/*
+async function getUser(email: string): Promise<User | undefined> {
+  try {
+    await ensureUsersTableExists();
+
+    const { rows } = await sql<User>`SELECT * FROM users WHERE email=${email}`;
+    return rows[0];
+  } catch (error) {
+    console.error("Failed to fetch user:", error);
+    throw new Error("Failed to fetch user.");
+  }
+}
+*/
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Credentials({
       name: "Credentials",
       credentials: {
-        username: { label: "Username", type: "text" },
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "john@example.com",
+        },
         password: { label: "Password", type: "password" },
       },
-      async authorize() {
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+        if (!parsedCredentials.success) {
+          throw new Error("Invalid credentials");
+        }
+        const { email, password } = parsedCredentials.data;
+
         // Add logic here to look up the user from your database
-        const user = { id: 1, name: "J Smith", email: "jsmith@example.com" };
-        if (user) {
+        // const user = await getUser(email);
+        const user = { id: 1, name: "new user", email: email };
+
+        if (!user) {
+          // No user found, so this is their first attempt to login
+          // meaning this is also the place you could do registration
+          throw new Error("User not found");
+        }
+        // Check if the provided password matches the stored one.
+        /* const passwordsMatch = await bcrypt.compare(password, user.password);
+           if (passwordsMatch) {
+            return user;
+          } else {
+            throw new Error("Invalid password");
+          } */
+        if (
+          parsedCredentials.data.email === user.email &&
+          parsedCredentials.data.password === password
+        ) {
           return user;
         } else {
-          return null;
+          throw new Error("Invalid credentials");
         }
       },
     }),
---
+++ lib/actions.ts
@@ -0,0 +1,207 @@
+// lib/actions.ts
+
+"use server";
+
+import { z } from "zod";
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { signIn } from "@/auth";
+import AuthError from "next-auth";
+import { hash } from "bcrypt";
+
+export async function ensureUsersTableExists() {
+  await sql`
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      email VARCHAR(255) UNIQUE NOT NULL,
+      role VARCHAR(255) NOT NULL,
+      password VARCHAR(255) NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+  `;
+}
+
+// Define validation schema for signup form
+const SignupSchema = z.object({
+  name: z.string().min(1, { message: "Name is required" }),
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters long" }),
+});
+
+export async function registerUser(formData: FormData) {
+  const validatedFields = SignupSchema.safeParse({
+    name: formData.get("name"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Validation failed",
+    };
+  }
+
+  const { name, email, password } = validatedFields.data;
+  const userRole = "user";
+  const hashedPassword = await hash(password, 12);
+
+  try {
+    await sql`
+      INSERT INTO users (name, email, role, password)
+      VALUES (${name}, ${email}, ${userRole}, ${hashedPassword})
+    `;
+    revalidatePath("/dashboard/users");
+    await signIn("credentials", formData, { redirectTo: "/home" });
+
+    return; // Success
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData, { redirectTo: "/home" });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        case "CallbackRouteError":
+          return `${error}`;
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
+}
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string({
+    invalid_type_error: "Please select a customer.",
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: "Please enter an amount greater than $0." }),
+  status: z.enum(["pending", "paid"], {
+    invalid_type_error: "Please select an invoice status.",
+  }),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
+  // Validate form using Zod
+  const validatedFields = CreateInvoice.safeParse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Create Invoice.",
+    };
+  }
+
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+
+  // Insert data into the database
+  try {
+    await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+      `;
+  } catch (error) {
+    // If a database error occurs, return a more specific error.
+    return {
+      message: `Database Error: Failed to Create Invoice: ${error.message}`,
+    };
+  }
+
+  // Revalidate the cache for the invoices page and redirect the user.
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData
+) {
+  const validatedFields = UpdateInvoice.safeParse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Update Invoice.",
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
+  const amountInCents = amount * 100;
+
+  try {
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+  } catch (error) {
+    return {
+      message: `Database Error: Failed to Update Invoice: ${error.message}`,
+    };
+  }
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+export async function deleteInvoice(id: string) {
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath("/dashboard/invoices");
+    return { message: "Deleted Invoice." };
+  } catch (error) {
+    return {
+      message: `Database Error: Failed to Delete Invoice: ${error.message}`,
+    };
+  }
+}
